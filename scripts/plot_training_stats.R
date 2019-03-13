3e28dc49c1fd2c193503ab42663c7b30f346ef91
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2019-03-13 16:28:25 +0100

    add scripts

---
+++ scripts/plot_training_stats.R
@@ -0,0 +1,143 @@
+#!/usr/bin/env Rscript
+
+#' script for plotting training data
+#' ---------------------------------
+#' takes two arguments, the path to the training data file and the output file.
+#' if training remotely, it can be a good idea to mount the output directory
+#' with `sshfs`.
+
+library(tidyverse)
+library(ggpubr)
+library(ggrepel)
+library(grid)
+library(gridExtra)
+library(gtable)
+library(zeallot)
+
+
+take <- function(n, xs) {
+    if (n == 0 || length(xs) == 0) {
+        return(list(list(), xs))
+    }
+    c(rest, ys) %<-% take(n - 1, tail(xs, -1))
+    return(list(c(head(xs, 1), rest), ys))
+}
+
+
+chunksOf <- function(n, xs) {
+    if (length(xs) == 0) {
+        return(list())
+    }
+    c(ys, zs) %<-% take(n, xs)
+    zs_ <- chunksOf(n, zs)
+    return(c(list(ys), zs_))
+}
+
+
+gracePeriod <- 600
+
+args <- commandArgs(trailingOnly = T)
+stopifnot(length(args) > 0 && length(args) <= 2)
+
+inputFile <- args[1]
+outputFile <- (
+    if (length(args) == 2) {
+        args[2]
+    } else {
+        bns <- strsplit(basename(inputFile), '\\.')[[1]]
+        bn <- (
+            if (length(bns) == 1) bns
+            else invoke(paste, head(bns, -1), sep='.')
+        )
+        file.path(dirname(inputFile), sprintf('%s.pdf', bn))
+    }
+)
+
+message(sprintf('input file=%s, output file=%s', inputFile, outputFile))
+
+repeat {
+    message('reading data...')
+    df = (
+        read.csv(inputFile, col.names=c('epoch', 'iteration', 'type', 'value'))
+        %>% as_tibble()
+        %>% filter(!is.nan(value))
+    )
+
+    message('making plot...')
+    c(plots, legends) %<-% (
+        unique(df$type)
+        %>% map(function(t) {
+            data <- (
+                df
+                %>% filter(type == t, epoch > 0.1 * dplyr::last(epoch))
+                %>% group_by(epoch)
+                %>% summarize(
+                        v=mean(value)
+                      , min=quantile(value, 0.25)
+                      , max=quantile(value, 0.75)
+                    )
+            )
+            annotations <- (
+                data
+                %>% summarize(min=min(v) , max=max(v))
+                %>% gather(key, v)
+            )
+            plot <- (
+                ggplot(data)
+                + aes(
+                      epoch
+                    , v
+                    , ymin=min
+                    , ymax=max
+                  )
+                + geom_point(size=0.5)
+                + geom_ribbon(alpha=0.15, linetype='blank')
+                + geom_smooth(linetype='solid', size=0.3, se=F)
+                + geom_hline(
+                      aes(yintercept=v)
+                    , annotations
+                    , linetype='dashed'
+                    , size=0.15
+                  )
+                + geom_text_repel(
+                      aes(
+                        , y=v
+                        , label=format(v, digits=3)
+                      )
+                    , annotations
+                    , x=min(data$epoch)
+                    , hjust=0
+                    , alpha=0.8
+                    , fontface='bold'
+                    , size=2
+                    , direction='y'
+                    , inherit.aes=F
+                  )
+                + ylab(t)
+                + theme_minimal()
+                + theme_bw()
+                + rremove('grid')
+            )
+            legend <- get_legend(plot)
+            return(list(plot + rremove('legend'), legend))
+        })
+        %>% transpose()
+    )
+
+    message('saving plot...')
+    # png(outputFile, height=5, width=10, units='in', res=200)
+    pdf(outputFile, height=5, width=10)
+    grid.draw(
+        chunksOf(ceiling(sqrt(length(plots))), plots)
+        %>% map(function(xs) {
+            top <- head(xs, -1) %>% map(~. + rremove('x.title'))
+            do.call(gtable_rbind, c(top, tail(xs, 1)) %>% map(ggplotGrob))
+        })
+        %>% invoke(arrangeGrob, ., nrow = 1)
+        # %>% arrangeGrob(first(legends), widths=c(0.9, 0.1))
+    )
+    dev.off()
+
+    message(sprintf('done. now sleeping for %ds', gracePeriod))
+    Sys.sleep(gracePeriod)
+}
---
+++ scripts/prepare_data.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+
+import itertools as it
+
+import os
+
+from imageio import imread, imwrite
+
+import numpy as np
+
+import pandas as pd
+
+from tqdm import tqdm
+
+
+Spot = namedtuple('Spot', ['x', 'y', 'r'])
+
+
+def run(image, counts, spots):
+    label = np.zeros(image.shape[:2]).astype(np.int64)
+
+    print('filling labels')
+    iterator = tqdm(spots, dynamic_ncols=True)
+    for i, s in enumerate(iterator, 1):
+        x, y, r = [int(round(x)) for x in (s.x, s.y, s.r)]
+        label[
+            tuple(zip(*(
+                (y - dy, x - dx)
+                for dy, dx in
+                filter(
+                    lambda x: np.sum(np.array(x) ** 2) <= s.r ** 2,
+                    it.product(
+                        range(-r, r + 1),
+                        range(-r, r + 1),
+                    )
+                )
+            )))
+        ] = i
+
+    cs = [[s.x, s.y] for s in spots]
+
+    xmin, ymin = np.min(cs, 0)
+    xmax, ymax = np.max(cs, 0)
+
+    xmin -= 0.1 * (xmax - xmin)
+    xmax += 0.1 * (xmax - xmin)
+    ymin -= 0.1 * (ymax - ymin)
+    ymax += 0.1 * (ymax - ymin)
+
+    xmin, xmax, ymin, ymax = [
+        int(round(x)) for x in (xmin, xmax, ymin, ymax)]
+
+    return (
+        counts,
+        image[ymin:ymax, xmin:xmax],
+        label[ymin:ymax, xmin:xmax],
+    )
+
+
+def main():
+    import argparse as ap
+
+    parser = ap.ArgumentParser()
+    parser.add_argument('--image')
+    parser.add_argument('--counts')
+    parser.add_argument('--spots')
+    parser.add_argument('--output-directory')
+
+    opts = vars(parser.parse_args())
+
+    output_directory = opts.pop('output_directory')
+    os.makedirs(output_directory)
+
+    # image = imread(opts.pop('image'))
+    counts = pd.read_csv(opts.pop('counts'), sep='\t', index_col=0)
+    spots = opts.pop('spots')
+    if spots is not None:
+        spots = pd.read_csv(spots, sep='\t')
+        counts = counts.loc[
+            spots[['x', 'y']]
+            .apply(lambda x: 'x'.join(map(str, x)), 1)
+        ]
+        xmax, xmin = [f(spots.x) for f in (np.max, np.min)]
+        pxmax, pxmin = [
+            np.mean(spots.pixel_x[spots.x == x]) for x in (xmax, xmin)]
+        r = (pxmax - pxmin) / (xmax - xmin) / 4
+        spots = list(
+            spots[['pixel_x', 'pixel_y']]
+            .apply(lambda x: Spot(*x, r), 1)
+        )
+    else:
+        r = np.sqrt(np.product(image.shape[:2]) / 32 / 34) / 4
+        spots = [
+            Spot(
+                *[
+                    round((float(y) - 1) / d * s)
+                    for y, s, d in zip(
+                        x.split('x'), image.shape[:2][::-1], (32, 34))
+                ],
+                r,
+            )
+            for x in counts.index
+        ]
+
+    counts = counts.reset_index().rename({'index': 'n'}, axis='columns')
+    counts['n'] = [*range(1, len(counts) + 1)]
+
+    counts_, image_, label = run(image, counts, spots)
+
+    print('writing counts...')
+    counts_.to_csv(
+        os.path.join(output_directory, 'data.csv.gz'),
+        sep=',',
+        index=False,
+    )
+    print('writing image...')
+    imwrite(os.path.join(output_directory, 'image.tif'), image_)
+    print('writing labels...')
+    imwrite(os.path.join(output_directory, 'label.tif'), label)
+
+
+if __name__ == '__main__':
+    main()
