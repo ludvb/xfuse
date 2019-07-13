b1543c88fe85fa905749590eab43a9d49d7663ff
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2019-07-13 11:23:35 +0200

    fix param_store setter

--- hssl/session/item/param_store.py
+++ hssl/session/item/param_store.py
@@ -9,6 +9,8 @@ _DEFAULT_STORE = p.poutine.runtime._PYRO_PARAM_STORE
 def _setter(store):
     p.poutine.runtime._PYRO_PARAM_STORE = store
     p.primitives._PYRO_PARAM_STORE = store
+    p.primitives._param = p.poutine.runtime.effectful(
+        store.get_param, type='param')
 
 
 param_store = SessionItem(setter=_setter, default=_DEFAULT_STORE)
