d3f5f1488105f8a86b56714a2480f6551189647e
Ludvig Bergenstråhle <ludwigbe@kth.se>
2019-10-21 15:27:22 +0200

    add session restoration test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -2,7 +2,10 @@ r"""Functional tests"""
 
 import os
 
+import pyro
 import pytest
+
+from hssl.session import Session, Unset, get
 
 
 @pytest.mark.parametrize(
@@ -26,3 +29,33 @@ def test_train_exit_status(shared_datadir, script_runner, tmp_path, arguments):
     assert "final.session" in os.listdir(save_path)
     assert "log" in os.listdir(save_path)
     assert "stats" in os.listdir(save_path)
+
+
+def test_restore_session(shared_datadir, script_runner, mocker, tmp_path):
+    """Test session restore"""
+    subcmd = [
+        "train",
+        str(shared_datadir / "test1" / "design.csv"),
+        "--patch-size=32",
+        "--batch-size=1",
+        "--epochs=1",
+    ]
+
+    script_runner.run("hssl", f"--save-path={tmp_path}", *subcmd)
+
+    params = [*pyro.get_param_store().values()]
+    pyro.clear_param_store()
+
+    def _mock_run_training(*_args, **_kwargs):
+        with Session(panic=Unset):
+            assert get("global_step").value > 1
+            assert all(
+                (a == b).all()
+                for a, b in zip(params, get("param_store").values())
+            )
+
+    mocker.patch("hssl.__main__.run_training", _mock_run_training)
+
+    assert script_runner.run(
+        "hssl", f"--session={tmp_path / 'final.session'}", *subcmd
+    ).success
