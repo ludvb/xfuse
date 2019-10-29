5ec714ca18dfee4e60d68e4ea49e72446b7b5c20
Ludvig Bergenstråhle <ludwigbe@kth.se>
2019-10-29 11:32:59 +0100

    evaluate factors on ELBO instead of expected likelihood

--- hssl/model/utility/model_comparison.py
+++ hssl/model/utility/model_comparison.py
@@ -5,17 +5,18 @@ import pyro as p
 
 def compare(data, guide, *models, num_samples=1):
     r"""
-    Returns the log-likelihood of given models on provided data using the same
-    guide trace
+    Returns the ELBO of given models on provided data using the same guide
+    trace
     """
 
     def _once():
         def _evaluate(model):
             with p.poutine.trace() as trace:
-                with p.poutine.block(hide_fn=lambda x: not x["is_observed"]):
-                    with p.poutine.replay(trace=guide):
-                        model(data)
-            return trace.trace.log_prob_sum().item()
+                with p.poutine.replay(trace=guide):
+                    model(data)
+            return trace.trace.log_prob_sum().item() - guide.log_prob_sum(
+                site_filter=lambda name, site: name in trace.trace.nodes
+            )
 
         return [_evaluate(model) for model in models]
 
