b806599d795d51166bd911c2469b74b9877ad834
Ludvig Bergenstråhle <ludwigbe@kth.se>
2019-11-21 17:10:40 +0100

    start global_step from 1

--- hssl/session/items/global_step.py
+++ hssl/session/items/global_step.py
@@ -4,7 +4,7 @@ from .. import SessionItem, register_session_item
 class GlobalStep:
     r"""Holds the value for the global step"""
 
-    def __init__(self, value=0):
+    def __init__(self, value=1):
         self.value = value
 
     def __iadd__(self, n):
