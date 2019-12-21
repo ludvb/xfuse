211592e94c72bb9b79217301b32265777fb6a532
Ludvig Bergenstråhle <ludwigbe@kth.se>
2019-12-21 11:32:05 +0100

    Revert "simplify architecture"
    This reverts commit bc22a954896aa408961f7ff689155c1d2f637d73.

--- xfuse/model/experiment/image.py
+++ xfuse/model/experiment/image.py
@@ -39,11 +39,6 @@ class Image(Experiment):
                     ),
                     torch.nn.BatchNorm2d(y.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
-                    torch.nn.Conv2d(
-                        y.shape[1], y.shape[1], kernel_size=3, padding=1
-                    ),
-                    torch.nn.BatchNorm2d(y.shape[1]),
-                    torch.nn.LeakyReLU(0.2, inplace=True),
                 ),
             ).to(y)
             return decoder(y)
@@ -83,6 +78,29 @@ class Image(Experiment):
         for i, z in zip(reversed(range(1, self.depth)), zs[::-1][1:]):
             y = _decode(_combine(_upsample(y, i), z, i), i)
 
+        postdecoder = get_module(
+            "postdecoder",
+            lambda: torch.nn.Sequential(
+                torch.nn.Conv2d(
+                    self.num_channels,
+                    self.num_channels,
+                    kernel_size=3,
+                    padding=1,
+                ),
+                torch.nn.BatchNorm2d(self.num_channels),
+                torch.nn.LeakyReLU(0.2, inplace=True),
+                torch.nn.Conv2d(
+                    self.num_channels,
+                    self.num_channels,
+                    kernel_size=3,
+                    padding=1,
+                ),
+                torch.nn.BatchNorm2d(self.num_channels),
+                torch.nn.LeakyReLU(0.2, inplace=True),
+            ).to(y),
+        ).to(y)
+        y = postdecoder(y)
+
         return y
 
     def _encode(self, x):
@@ -109,11 +127,7 @@ class Image(Experiment):
                 f"downsampler-{i}",
                 lambda: torch.nn.Sequential(
                     torch.nn.Conv2d(
-                        x.shape[1],
-                        2 * x.shape[1],
-                        kernel_size=4,
-                        stride=2,
-                        padding=1,
+                        x.shape[1], 2 * x.shape[1], kernel_size=2, stride=2
                     ),
                     torch.nn.BatchNorm2d(2 * x.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
