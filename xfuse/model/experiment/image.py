85b85ee93055b2dc742d5098a528132514590f42
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2019-12-28 17:11:28 +0100

    Revert "try out activation normalization"
    This reverts commit be7d842797e57058a80a746a1b2c9eb46ed1a8d6.

--- xfuse/model/experiment/image.py
+++ xfuse/model/experiment/image.py
@@ -1,6 +1,6 @@
 import pyro
 import torch
-from actnorm import ActNorm2d
+from batchrenorm import BatchRenorm2d
 from pyro.distributions import Normal  # pylint: disable=no-name-in-module
 
 from ...utility import center_crop
@@ -33,12 +33,12 @@ class Image(Experiment):
                     torch.nn.Conv2d(
                         y.shape[1], y.shape[1], kernel_size=3, padding=1
                     ),
-                    ActNorm2d(y.shape[1]),
+                    BatchRenorm2d(y.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                     torch.nn.Conv2d(
                         y.shape[1], y.shape[1], kernel_size=3, padding=1
                     ),
-                    ActNorm2d(y.shape[1]),
+                    BatchRenorm2d(y.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                 ),
             ).to(y)
@@ -54,7 +54,7 @@ class Image(Experiment):
                         kernel_size=3,
                         padding=1,
                     ),
-                    ActNorm2d(z.shape[1]),
+                    BatchRenorm2d(z.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                 ),
             ).to(y)
@@ -71,7 +71,7 @@ class Image(Experiment):
                     torch.nn.Conv2d(
                         y.shape[1], y.shape[1] // 2, kernel_size=5, padding=2
                     ),
-                    ActNorm2d(y.shape[1] // 2),
+                    BatchRenorm2d(y.shape[1] // 2),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                 ),
             ).to(y)
@@ -91,12 +91,12 @@ class Image(Experiment):
                     torch.nn.Conv2d(
                         x.shape[1], x.shape[1], kernel_size=3, padding=1
                     ),
-                    ActNorm2d(x.shape[1]),
+                    BatchRenorm2d(x.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                     torch.nn.Conv2d(
                         x.shape[1], x.shape[1], kernel_size=3, padding=1
                     ),
-                    ActNorm2d(x.shape[1]),
+                    BatchRenorm2d(x.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                 ).to(x),
             ).to(x)
@@ -113,7 +113,7 @@ class Image(Experiment):
                         stride=2,
                         padding=2,
                     ),
-                    ActNorm2d(2 * x.shape[1]),
+                    BatchRenorm2d(2 * x.shape[1]),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                 ).to(x),
             ).to(x)
@@ -125,7 +125,7 @@ class Image(Experiment):
                 torch.nn.Conv2d(
                     x.shape[1], self.num_channels, kernel_size=3, padding=1
                 ),
-                ActNorm2d(self.num_channels),
+                BatchRenorm2d(self.num_channels),
                 torch.nn.LeakyReLU(0.2, inplace=True),
             ).to(x),
         ).to(x)
@@ -142,7 +142,7 @@ class Image(Experiment):
                 torch.nn.Conv2d(
                     self.num_channels, self.num_channels, kernel_size=1
                 ),
-                ActNorm2d(self.num_channels),
+                BatchRenorm2d(self.num_channels),
                 torch.nn.LeakyReLU(0.2, inplace=True),
                 torch.nn.Conv2d(
                     self.num_channels, x["image"].shape[1], kernel_size=1
@@ -159,7 +159,7 @@ class Image(Experiment):
                 torch.nn.Conv2d(
                     self.num_channels, self.num_channels, kernel_size=1
                 ),
-                ActNorm2d(self.num_channels),
+                BatchRenorm2d(self.num_channels),
                 torch.nn.LeakyReLU(0.2, inplace=True),
                 torch.nn.Conv2d(
                     self.num_channels, x["image"].shape[1], kernel_size=1
--- xfuse/model/experiment/st/st.py
+++ xfuse/model/experiment/st/st.py
@@ -7,7 +7,7 @@ import numpy as np
 import pyro as p
 import torch
 import torch.distributions.constraints as constraints
-from actnorm import ActNorm2d
+from batchrenorm import BatchRenorm2d
 from pyro.contrib.autoname import scope
 from pyro.distributions import (  # pylint: disable=no-name-in-module
     Delta,
@@ -134,7 +134,7 @@ class ST(Image):
             dataset = require("dataloader").dataset
             decoder = torch.nn.Sequential(
                 torch.nn.Conv2d(in_channels, in_channels, kernel_size=1),
-                ActNorm2d(in_channels),
+                BatchRenorm2d(in_channels),
                 torch.nn.LeakyReLU(0.2, inplace=True),
                 torch.nn.Conv2d(in_channels, 1, kernel_size=1),
                 torch.nn.Softplus(),
@@ -162,7 +162,7 @@ class ST(Image):
                 "metagene_shared",
                 lambda: torch.nn.Sequential(
                     torch.nn.Conv2d(in_channels, in_channels, kernel_size=1),
-                    ActNorm2d(in_channels),
+                    BatchRenorm2d(in_channels),
                     torch.nn.LeakyReLU(0.2, inplace=True),
                 ),
             ),
