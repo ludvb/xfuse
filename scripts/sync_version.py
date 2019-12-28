a4377b3514034790c1869373fd8623a3517841d1
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2019-12-28 16:07:00 +0100

    update build system

---
+++ scripts/sync_version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# pylint: disable=invalid-name
+
+import os
+import sys
+
+import tomlkit
+
+
+project_file = sys.argv[1]
+
+with open(project_file, "r") as fp:
+    project_config = tomlkit.loads(fp.read())
+
+version = project_config["tool"]["poetry"]["version"]
+
+with open(
+    os.path.join(os.path.dirname(project_file), "xfuse", "__version__.py"), "w"
+) as fp:
+    fp.write(f'__version__ = "{version}"\n')
