7ed06f88ec72dc26344a2f9226be67b69b087a9b
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2020-01-02 17:28:19 +0100

    :bug: set active genes on dataset, not slides

--- xfuse/session/items/genes.py
+++ xfuse/session/items/genes.py
@@ -6,8 +6,7 @@ from .. import SessionItem, get, register_session_item
 def _set_genes(x: Optional[List[str]]) -> None:
     dataloader = get("dataloader")
     if dataloader and x:
-        for slide in dataloader.dataset.data.slides.values():
-            slide.data.genes = x
+        dataloader.dataset.genes = x
 
 
 register_session_item("genes", SessionItem(setter=_set_genes, default=None))
