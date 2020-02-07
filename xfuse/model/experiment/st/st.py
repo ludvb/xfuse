5e583b3086bf5762e733c88c0e5da8263e501e61
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2020-02-07 13:39:35 +0100

    keep split metagene activation constant

--- xfuse/model/experiment/st/st.py
+++ xfuse/model/experiment/st/st.py
@@ -125,7 +125,12 @@ class ST(Image):
         ]:
             new_mname = mname.replace(name, new_name)
             log(DEBUG, "copying module: %s -> %s", mname, new_mname)
-            state_dict.modules[new_mname] = state_dict.modules[mname]
+            state_dict.modules[new_mname] = deepcopy(state_dict.modules[mname])
+
+        # adjust bias so that the summed exponential activation of the two new
+        # metagenes is the same as the activation before splitting
+        state_dict.modules[f"decoder_{name}"]["0.bias"] -= np.log(2)
+        state_dict.modules[f"decoder_{new_name}"]["0.bias"] -= np.log(2)
 
         load_state_dict(state_dict)
 
