6d6607877fc8f18e02a80c2a02d29e133cca743a
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2020-02-08 13:47:24 +0100

    remove summary stats

--- xfuse/data/slide/slide.py
+++ xfuse/data/slide/slide.py
@@ -77,19 +77,6 @@ class STSlide(SlideData):
     def type(self) -> str:
         return "ST"
 
-    def __recompute_summary_statistics(self):
-        try:
-            counts = self.counts[
-                ~np.isin(np.arange(self.counts.shape[0]), self.__label_mask)
-            ]
-        except AttributeError:
-            counts = self.counts
-        counts = counts.todense()
-        # pylint: disable=attribute-defined-outside-init
-        self.__counts_means = np.mean(np.array(counts), 0)
-        self.__counts_stdvs = np.nanstd(np.array(counts), 0)
-        return self
-
     @property
     def min_counts(self) -> float:
         r"""
@@ -119,7 +106,6 @@ class STSlide(SlideData):
                 self._data.filename,
                 ", ".join(map(str, self.__label_mask)),
             )
-        self.__recompute_summary_statistics()
 
     @property
     def genes(self):
@@ -135,7 +121,6 @@ class STSlide(SlideData):
         self.__gene_idxs = np.array(
             [idxs[gene] if gene in idxs else -1 for gene in genes]
         )
-        self.__recompute_summary_statistics()
         return self
 
     def __construct_count_matrix(self):
@@ -163,16 +148,6 @@ class STSlide(SlideData):
         else:
             counts = self.__construct_count_matrix()
         return counts[:, self.__gene_idxs]
-
-    @property
-    def means(self):
-        r"""Count mean for each gene"""
-        return self.__counts_means
-
-    @property
-    def stdvs(self):
-        r"""Count standard deviation for each gene"""
-        return self.__counts_stdvs
 
     @property
     def image(self):
@@ -207,8 +182,6 @@ class STSlide(SlideData):
             ),
             label=torch.as_tensor(label).long(),
             data=torch.as_tensor(data.todense()).float(),
-            means=torch.as_tensor(self.means).float(),
-            stdvs=torch.as_tensor(self.stdvs).float(),
         )
 
 
