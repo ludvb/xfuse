815cdd4de547f0c1e7e10063ecd5e5bc9e1c0534
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2020-10-31 21:17:09 +0100

    refactor cli

--- xfuse/__main__.py
+++ xfuse/__main__.py
@@ -8,7 +8,6 @@ import sys
 import warnings
 from contextlib import ExitStack
 from datetime import datetime as dt
-from functools import wraps
 
 import click
 import h5py
@@ -29,7 +28,7 @@ from .model.experiment.st.metagene_expansion_strategy import (
     STRATEGIES as expansion_strategies,
 )
 from .run import run as _run
-from .session import Session, get
+from .session import Session, get, require
 from .utility import design_matrix_from, temp_attr, with_
 from .utility.file import first_unique_filename
 from .session.io import load_session
@@ -37,25 +36,41 @@ from .session.io import load_session
 
 _DEFAULT_SESSION = Session()
 
-sys.excepthook = lambda *_: None
-
 
 def _init(f):
-    @wraps(f)
     @with_(_DEFAULT_SESSION)
-    def _wrapped(*args, **kwargs):
+    @click.option(
+        "--save-path",
+        type=click.Path(),
+        default=f"xfuse-{dt.now().isoformat()}",
+        help="The output path",
+        show_default=True,
+    )
+    @click.option("--debug", is_flag=True)
+    def _wrapped(*args, debug, save_path, **kwargs):
         logging.captureWarnings(True)
-        return f(*args, **kwargs)
+        os.makedirs(save_path, exist_ok=True)
+        log_filename = first_unique_filename(os.path.join(save_path, "log"))
+        with open(log_filename, "w") as log_file:
+            with Session(
+                save_path=save_path,
+                log_file=[sys.stderr, log_file],
+                log_level=DEBUG if debug else INFO,
+            ):
+                with temp_attr(sys, "excepthook", lambda *_: None):
+                    log(
+                        INFO, "This is %s version %s", __package__, __version__
+                    )
+                    log(DEBUG, "Invoked by `%s`", " ".join(sys.argv))
+                    return f(*args, **kwargs)
 
     return _wrapped
 
 
 @click.group()
-@click.option("--debug", is_flag=True)
 @click.version_option()
-def cli(debug):
-    if debug:
-        _DEFAULT_SESSION.log_level = -999
+def cli():
+    pass
 
 
 @click.group("convert")
@@ -75,11 +90,6 @@ cli.add_command(_convert)
 @click.option("--scale", type=float)
 @click.option("--mask/--no-mask", default=True)
 @click.option("--rotate/--no-rotate", default=False)
-@click.option(
-    "--output-file",
-    type=click.Path(exists=False, writable=True),
-    required=True,
-)
 @_init
 def _convert_visium(
     image,
@@ -90,9 +100,10 @@ def _convert_visium(
     scale,
     mask,
     rotate,
-    output_file,
 ):
     r"""Converts 10X Visium data"""
+    save_path = require("save_path")
+
     tissue_positions = pd.read_csv(tissue_positions, index_col=0, header=None)
     tissue_positions = tissue_positions[[4, 5]]
     tissue_positions = tissue_positions.rename(columns={4: "y", 5: "x"})
@@ -108,6 +119,8 @@ def _convert_visium(
             annotation = {
                 k: annotation_file[k][()] for k in annotation_file.keys()
             }
+
+    output_file = os.path.join(save_path, "data.h5")
 
     with h5py.File(bc_matrix, "r") as data:
         convert.visium.run(
@@ -135,11 +148,6 @@ _convert.add_command(_convert_visium, "visium")
 @click.option("--scale", type=float)
 @click.option("--mask/--no-mask", default=True)
 @click.option("--rotate/--no-rotate", default=False)
-@click.option(
-    "--output-file",
-    type=click.Path(exists=False, writable=True),
-    required=True,
-)
 @_init
 def _convert_st(
     counts,
@@ -150,31 +158,40 @@ def _convert_st(
     scale,
     mask,
     rotate,
-    output_file,
 ):
     r"""Converts Spatial Transcriptomics ("ST") data"""
+    save_path = require("save_path")
+
     if spots is not None and transformation_matrix is not None:
         raise RuntimeError(
             "Please pass either a spot data file or a text file containing a"
             " transformation matrix"
         )
+
     if spots is not None:
         spots_data = pd.read_csv(spots, sep="\t")
     else:
         spots_data = None
+
     if transformation_matrix is not None:
         transformation = np.loadtxt(transformation_matrix)
         transformation = transformation.reshape(3, 3)
     else:
         transformation = None
+
     counts_data = pd.read_csv(counts, sep="\t", index_col=0)
+
     with temp_attr(Image, "MAX_IMAGE_PIXELS", None):
         image_data = imread(image)
+
     if annotation:
         with h5py.File(annotation, "r") as annotation_file:
             annotation = {
                 k: annotation_file[k][()] for k in annotation_file.keys()
             }
+
+    output_file = os.path.join(save_path, "data.h5")
+
     convert.st.run(
         counts_data,
         image_data,
@@ -197,26 +214,26 @@ _convert.add_command(_convert_st, "st")
 @click.option("--scale", type=float)
 @click.option("--mask/--no-mask", default=True)
 @click.option("--rotate/--no-rotate", default=False)
-@click.option(
-    "--output-file",
-    type=click.Path(exists=False, writable=True),
-    required=True,
-)
 @_init
 def _convert_image(
-    image, annotation, scale, mask, rotate, output_file,
+    image, annotation, scale, mask, rotate,
 ):
     r"""Converts image without any associated expression data"""
+    save_path = require("save_path")
+
     with temp_attr(Image, "MAX_IMAGE_PIXELS", None):
         image_data = imread(image)
+
     if annotation:
         with h5py.File(annotation, "r") as annotation_file:
             annotation = {
                 k: annotation_file[k][()] for k in annotation_file.keys()
             }
+
+    output_file = os.path.join(save_path, "data.h5")
+
     convert.image.run(
         image_data,
-        output_file,
         annotation=annotation,
         scale_factor=scale,
         mask=mask,
@@ -232,7 +249,6 @@ _convert.add_command(_convert_image, "image")
 @click.argument(
     "slides", type=click.Path(exists=True, dir_okay=False), nargs=-1
 )
-@_init
 def init(target, slides):
     r"""Creates a template for the project configuration file."""
     config = construct_default_config_toml()
@@ -247,28 +263,17 @@ cli.add_command(init)
 
 @click.command()
 @click.argument("project-file", type=click.File("rb"))
-@click.option(
-    "--save-path",
-    type=click.Path(),
-    default=f"xfuse-{dt.now().isoformat()}",
-    help="The output path",
-    show_default=True,
-)
 @click.option("--session", type=click.File("rb"))
 @_init
-def run(project_file, save_path, session):
+def run(project_file, session):
     r"""
     Runs xfuse based on a project configuration file.
     The configuration file can be created manually or using the `init`
     subcommand.
     """
+    save_path = require("save_path")
     base_session = load_session(session) if session is not None else Session()
-    os.makedirs(save_path, exist_ok=True)
-    with open(
-        first_unique_filename(os.path.join(save_path, "log")), "w"
-    ) as log_file, base_session, Session(
-        save_path=save_path, log_file=[sys.stderr, log_file]
-    ):
+    with base_session:
         config = dict(tomlkit.loads(project_file.read().decode()))
         config = merge_config(config)
 
