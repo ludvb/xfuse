ludvb/xfuse

commit 61d2b0e77b4e595eec550521c056444e1d99e0a8

update dependencies

xfuse/data/slide/slide.py
@@ -110,7 +110,7 @@ class STSlide(SlideData):
 
     @genes.setter
     def genes(self, genes: List[str]) -> STSlide:
-        self.__gene_list = np.array(genes)
+        self.__gene_list = np.array(genes).astype(str)
         with h5py.File(self._datafile, "r") as data:
             idxs = {
                 gene: i for i, gene in enumerate(data["counts"]["columns"][()])

pyproject.toml
@@ -12,35 +12,35 @@ description = "Deep spatial data fusion"
 authors = ["Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>"]
 
 [tool.poetry.dependencies]
-click = "^7.0"
-h5py = "^2.10.0"
-imageio = "^2.6.1"
-matplotlib = "^3.1.2"
-numpy = "^1.18.0"
-pandas = "^0.25.3"
-Pillow = "^6.2.1"
-pyro-ppl = "^1.1.0"
-python = "^3.8.0"
-scikit-learn = "^0.22"
-scipy = "^1.4.1"
-tensorboard = "^2.1.0"
-tifffile = "2019.7.26"
-tomlkit = "^0.5.8"
-torch = "^1.5.0"
-torchvision = "^0.6.0"
-tqdm = "^4.41.0"
+click = "^7.1.2"
+h5py = "^3.0.0"
+imageio = "^2.9.0"
+matplotlib = "^3.3.2"
+numpy = "^1.19.4"
+opencv-python = "^4.4.0"
+pandas = "^1.1.4"
+Pillow = "^8.0.1"
+pyro-ppl = "^1.5.0"
+python = "^3.8"
+scikit-learn = "^0.23.2"
+scipy = "^1.5.4"
+tensorboard = "^2.3.0"
+tifffile = "^2020.10.1"
+tomlkit = "^0.7.0"
+torch = "^1.7.0"
+torchvision = "^0.8.1"
+tqdm = "^4.51.0"
 umap-learn = "^0.3.10"
-opencv-python = "^4.2.0"
-pylint = "^2.6.0"
-mypy = "^0.790"
 
 [tool.poetry.dev-dependencies]
-pre-commit = "^2.0.0"
-pytest = "^5.3.2"
-pytest-console-scripts = "^0.2.0"
-pytest-cov = "^2.8.1"
+mypy = "^0.790"
+pre-commit = "^2.8.2"
+pylint = "^2.6.0"
+pytest = "^6.1.2"
+pytest-console-scripts = "^1.0.0"
+pytest-cov = "^2.10.1"
 pytest-datadir = "^1.3.1"
-pytest-mock = "^1.13.0"
+pytest-mock = "^3.3.1"
 pytest-runner = "^5.2"
 
 [tool.poetry.scripts]

xfuse/analyze/gene_maps.py
@@ -53,7 +53,7 @@ def compute_gene_maps(
         shuffle=False,
     )
 
-    all_genes = np.array(genes)
+    all_genes = np.array(genes).astype(str)
     selected_genes = np.array(
         [
             x

xfuse/data/slide/random_slide.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import numpy as np
 from PIL import Image
@@ -44,7 +44,7 @@ class RandomSlide(SlideIterator):
 
     @staticmethod
     def _compute_extended_patch_size(
-        w: float, h: float, rotation: float, scale: float, shear: float
+        w: float, h: float, rotation: float, scale: float, shear: List[float]
     ) -> Tuple[float, float]:
         transform = np.concatenate(
             [
@@ -84,7 +84,7 @@ class RandomSlide(SlideIterator):
             np.random.uniform(-self._max_scale_jitter, self._max_scale_jitter)
         )
         shear = np.random.uniform(
-            -self._max_shear_jitter, self._max_shear_jitter
+            -self._max_shear_jitter, self._max_shear_jitter, size=2
         )
 
         # Compute the "extended" patch size. This is the size of the patch that

xfuse/model/experiment/experiment.py
@@ -36,5 +36,7 @@ class Experiment(t.nn.Module):
     def guide(self, x):
         r"""Experiment guide for :class:`pyro.infer.SVI`"""
 
-    def forward(self, x, zs):  # pylint: disable=arguments-differ
+    def forward(self, x, zs):
+        r"""Alias for :func:`model`"""
+        # pylint: disable=arguments-differ
         return self.model(x, zs)

xfuse/model/xfuse.py
@@ -67,6 +67,7 @@ class XFuse(torch.nn.Module):
         self.__experiment_store[experiment.tag] = experiment
 
     def forward(self, *input):
+        r"""Alias for :func:`model`"""
         # pylint: disable=redefined-builtin
         return self.model(*input)
 

xfuse/session/io.py
@@ -1,9 +1,8 @@
 import os
-import pickle
 import warnings
 from typing import Union
 
-import torch as t
+import torch
 
 from _io import BufferedReader
 
@@ -30,7 +29,8 @@ def save_session(filename_prefix: str) -> None:
 
     def _can_pickle(name, x):
         try:
-            pickle.dumps(x)
+            with open("/dev/null", "wb") as null:
+                torch.save(x, null)
         except Exception as exc:  # pylint: disable=broad-except
             warnings.warn(
                 f'Failed to store session item "{name}".'
@@ -50,7 +50,7 @@ def save_session(filename_prefix: str) -> None:
     )
 
     log(INFO, "Saving session to %s", path)
-    t.save((session, get_state_dict()), path)
+    torch.save((session, get_state_dict()), path)
 
 
 def load_session(file: Union[str, BufferedReader]) -> Session:
@@ -60,7 +60,7 @@ def load_session(file: Union[str, BufferedReader]) -> Session:
         "Loading session from %s",
         file.name if isinstance(file, BufferedReader) else file,
     )
-    session, state_dict = t.load(file, map_location="cpu")
+    session, state_dict = torch.load(file, map_location="cpu")
     with session:
         load_state_dict(state_dict)
     return session

xfuse/utility/tensor.py
@@ -69,8 +69,8 @@ def isoftplus(x, /):
     r"""
     Inverse softplus.
 
-    >>> ((isoftplus(torch.nn.functional.softplus(torch.linspace(-5, 5)))
-    ...     - torch.linspace(-5, 5)) < 1e-5).all()
+    >>> ((isoftplus(torch.nn.functional.softplus(torch.linspace(-5, 5, 10)))
+    ...     - torch.linspace(-5, 5, 10)) < 1e-5).all()
     tensor(True)
     """
     return np.log(np.exp(x) - 1)

commit f67b9503693bc3459cb2cd7140f77bab958a43b6

fix type regression from 61d2b0e

xfuse/data/slide/slide.py
@@ -61,7 +61,7 @@ class STSlide(SlideData):
         self._datafile = datafile
         with h5py.File(datafile, "r") as data:
             self.H, self.W, _ = data["image"].shape
-            self.genes = list(data["counts"]["columns"][()])
+            self.genes = list(data["counts"]["columns"][()].astype(str))
         self.__always_filter = always_filter or []
         self.__always_keep = always_keep or []
         self.cache_data = cache_data
@@ -110,10 +110,13 @@ class STSlide(SlideData):
 
     @genes.setter
     def genes(self, genes: List[str]) -> STSlide:
-        self.__gene_list = np.array(genes).astype(str)
+        self.__gene_list = np.array(genes)
         with h5py.File(self._datafile, "r") as data:
             idxs = {
-                gene: i for i, gene in enumerate(data["counts"]["columns"][()])
+                gene: i
+                for i, gene in enumerate(
+                    data["counts"]["columns"][()].astype(str)
+                )
             }
         self.__gene_idxs = np.array(
             [idxs[gene] if gene in idxs else -1 for gene in genes]