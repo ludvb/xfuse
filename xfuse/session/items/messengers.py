7bc70d0d96f520fa58face7f08b3c2d5fe765d45
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2020-11-19 11:41:59 +0100

    install messengers manually

--- xfuse/session/items/messengers.py
+++ xfuse/session/items/messengers.py
@@ -1,6 +1,6 @@
-from contextlib import ExitStack
 from typing import List
 
+import pyro.poutine.runtime as pyro_runtime
 from pyro.poutine.messenger import Messenger
 
 from .. import SessionItem, register_session_item
@@ -8,14 +8,23 @@ from .. import SessionItem, register_session_item
 
 __all__: List[str] = []
 
-
-__STACK = ExitStack()
+__INSTALLED_MESSENGERS: List[Messenger] = []
 
 
 def _messengers_setter(messengers: List[Messenger]) -> None:
-    __STACK.close()
-    for messenger in messengers:
-        __STACK.enter_context(messenger)
+    # pylint: disable=protected-access
+    # ^ HACK: This setter installs and uninstall Messengers manually by
+    #         modifying Pyro's runtime stack, effectively replacing their
+    #         __enter__ and __exit__ methods.
+    #         Messengers are always added to the bottom of the stack to avoid
+    #         interfering with other Messengers.
+    pyro_runtime._PYRO_STACK[:] = [
+        messenger
+        for messenger in pyro_runtime._PYRO_STACK
+        if messenger not in __INSTALLED_MESSENGERS
+    ]
+    pyro_runtime._PYRO_STACK[:] = [*messengers, *pyro_runtime._PYRO_STACK]
+    __INSTALLED_MESSENGERS[:] = messengers
 
 
 register_session_item(
