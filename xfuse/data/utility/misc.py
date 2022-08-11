f26bffec98bc849e034d3a337a51b4e29d22e938
Czarnewski <paulo.czarnewski@scilifelab.se>
2022-08-11 13:54:35 +0200

    fixes issue with Xfuses DataLoader

--- xfuse/data/utility/misc.py
+++ xfuse/data/utility/misc.py
@@ -92,77 +92,68 @@ def estimate_spot_size(dataset: Dataset) -> Dict[str, float]:
         )
     }
 
+def _worker_init(n):
+    np.random.seed(np.random.get_state()[1][0] + get("training_data").step)
+    np.random.seed(np.random.randint(np.iinfo(np.int32).max) + n)
+
+def _collate(xs):
+    def _remove_key(v):
+        v.pop("data_type")
+        return v
+
+    def _sort_key(x):
+        return x["data_type"]
+
+    def _collate(ys):
+        collated_data = {}
+
+        # we can't collate the count data as a tensor since its dimension
+        # will differ between samples. therefore, we return it as a list
+        # instead.
+        try:
+            collated_data.update({"data": [y.pop("data") for y in ys]})
+        except KeyError:
+            pass
+
+        # Collate any other non-tensor as list
+        collated_data.update(
+            {
+                k: [y.pop(k) for y in ys]
+                for k in set(
+                    k
+                    for y in ys
+                    for k, v in y.items()
+                    if not torch.is_tensor(v)
+                )
+            }
+        )
+
+        # Crop image sizes to the minimum size over the batch
+        min_size = {}
+        for y in ys:
+            for k, v in y.items():
+                if k in min_size:
+                    min_size[k] = torch.min(
+                        min_size[k], torch.as_tensor(v.shape)
+                    )
+                else:
+                    min_size[k] = torch.as_tensor(v.shape)
+        for y in ys:
+            for k, v in min_size.items():
+                y[k] = center_crop(y[k], v.numpy().tolist())
+        collated_data.update(default_collate(ys))
+
+        return collated_data
+
+    return {
+        k: _collate([_remove_key(v) for v in vs])
+        for k, vs in it.groupby(sorted(xs, key=_sort_key), key=_sort_key)
+    }
 
 def make_dataloader(dataset: Dataset, **kwargs: Any) -> DataLoader:
     r"""Creates a :class:`~torch.utils.data.DataLoader` for `dataset`"""
-
-    def _collate(xs):
-        def _remove_key(v):
-            v.pop("data_type")
-            return v
-
-        def _sort_key(x):
-            return x["data_type"]
-
-        def _collate(ys):
-            collated_data = {}
-
-            # we can't collate the count data as a tensor since its dimension
-            # will differ between samples. therefore, we return it as a list
-            # instead.
-            try:
-                collated_data.update({"data": [y.pop("data") for y in ys]})
-            except KeyError:
-                pass
-
-            # Collate any other non-tensor as list
-            collated_data.update(
-                {
-                    k: [y.pop(k) for y in ys]
-                    for k in set(
-                        k
-                        for y in ys
-                        for k, v in y.items()
-                        if not torch.is_tensor(v)
-                    )
-                }
-            )
-
-            # Crop image sizes to the minimum size over the batch
-            min_size = {}
-            for y in ys:
-                for k, v in y.items():
-                    if k in min_size:
-                        min_size[k] = torch.min(
-                            min_size[k], torch.as_tensor(v.shape)
-                        )
-                    else:
-                        min_size[k] = torch.as_tensor(v.shape)
-            for y in ys:
-                for k, v in min_size.items():
-                    y[k] = center_crop(y[k], v.numpy().tolist())
-            collated_data.update(default_collate(ys))
-
-            return collated_data
-
-        return {
-            k: _collate([_remove_key(v) for v in vs])
-            for k, vs in it.groupby(sorted(xs, key=_sort_key), key=_sort_key)
-        }
-
-    def _worker_init(n):
-        np.random.seed(np.random.get_state()[1][0] + get("training_data").step)
-        np.random.seed(np.random.randint(np.iinfo(np.int32).max) + n)
-
-    if np.isin(platform.system(),['Darwin','Windows']):
-        return torch.utils.data.DataLoader(
-        dataset = dataset,
-        collate_fn = torch.utils.data.dataloader.default_collate,
-        worker_init_fn = torch.utils.data.dataloader._worker_init_fn_t,
-        **kwargs )
-    else:
-        return DataLoader(
-        dataset = dataset,
-        collate_fn = _collate,
-        worker_init_fn = _worker_init,
-        **kwargs )
+    return DataLoader(
+    dataset = dataset,
+    collate_fn = _collate,
+    worker_init_fn = _worker_init,
+    **kwargs )
